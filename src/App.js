24600eeaf3aa29550649aeb73845c4d399740f5c
Yu Ling Cheng <yulingc@theodo.fr>
2018-11-24 02:25:12 +0100

    Init React app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <header className="header">
+          <h1>Frontend</h1>
+        </header>
+        <main />
+        <footer />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
